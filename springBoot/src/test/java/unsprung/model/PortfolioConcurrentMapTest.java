72ed8be98e4bf4ccc37a3fccca80285ba19a6be7
Uberto Barbini <uberto.gama@gmail.com>
2017-10-19 19:01:51 +0100

    added a test as example

---
+++ springBoot/src/test/java/unsprung/model/PortfolioConcurrentMapTest.java
@@ -0,0 +1,29 @@
+package unsprung.model;
+
+import org.junit.Test;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PortfolioConcurrentMapTest {
+
+    @Test
+    public void executeAnOrder() throws Exception {
+
+        String vod = "VOD.L";
+        Context ctx = new ContextMaps();
+        ctx.getStockPrices().put(vod, 123.4);
+        BigGlobals.init(ctx);
+
+        Portfolio p = new PortfolioConcurrentMap();
+
+        assertThat(p.getQuantity(vod)).isEqualTo(0);
+        assertThat(p.getValue(vod)).isEqualTo(0);
+
+        BuySellOrder order = new BuySellOrder(vod, 110);
+        p.executeOrder(order);
+
+        assertThat(p.getQuantity(vod)).isEqualTo(110);
+        assertThat(p.getValue(vod)).isEqualTo(13574);
+    }
+}
